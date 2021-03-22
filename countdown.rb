c0d7b94161a4c18e63d4f9d1bc338b7de6703989
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 00:35:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
-def countdown(integer)
+def countdown(number)
   counter = 0 
   
-  while 
+  while number > 0
     puts "#{number} SECOND(S)!"
     integer -= 1 
-  
+  end
 end
 
 puts "HAPPY NEW YEAR!"
